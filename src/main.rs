8cead9df0592123c2ebe14ddfc9b8a0f6d50be35
shanelonergan <sptlonergan@gmail.com>
2020-03-26 13:00:28 -0400

    integers. floats, and numeric operations

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,38 @@ fn main() {
     println!("The value of x is: {}", x);
 
     // constants
-    const MAX_POINTS: u32 = 100_000
+    const MAX_POINTS: u32 = 100_000;
+    println!("the most points you can get is {}", MAX_POINTS);
+
+    // shadowing
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+
+    // Integers and Floats
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    println!("The value of x is: {}. The value of y is: {}", x, y);
+
+    // NUMERIC OPERATIONS
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // BOOLEANS
 }
