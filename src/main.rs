cd120802ddd9e663e1d33e9b5fd622d84661de6b
shanelonergan <sptlonergan@gmail.com>
2020-03-26 13:26:03 -0400

    arrays

--- src/main.rs
+++ src/main.rs
@@ -49,14 +49,27 @@ fn main() {
     let heart_eyed_cat = '😻';
 
     // TUPLES
+    // collection of multiple items that can have different types
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let tup = (500, 6.4, 1);
     let (x, y, z) = tup; // destructuring
     println!("The value of y is: {}", y);
 
-    access individual values
+    // access individual values via index
     let x: (i32, f64, u8) = (500, 6.4, 1);
     let five_hundred = x.0;
     let six_point_four = x.1;
     let one = x.2;
+
+    // ARRAYS
+    // Every element must be the same type, and has a fixed length
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+                  "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [3; 5]; // same as let a = [3, 3, 3, 3, 3]
+
+    let a = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
 }
