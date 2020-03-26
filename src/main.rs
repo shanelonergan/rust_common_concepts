03d58678c371e053c7e61642f931f2f90f554321
shanelonergan <sptlonergan@gmail.com>
2020-03-26 12:42:06 -0400

    constant

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
+    // standard variables
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // constants
+    const MAX_POINTS: u32 = 100_000
 }
